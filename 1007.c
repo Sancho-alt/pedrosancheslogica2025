ff580bb49ac35db0b9dc2c40d736fb72b8afef67
Sancho-alt <pedro.sanches@unifesp.br>
2025-05-23 14:20:25 -0300

    1007.c

---
+++ 1007.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int A = 0, B = 0, C = 0, D = 0, Dif = 0;
+    scanf("%d", &A);
+    scanf("%d", &B);
+    scanf("%d", &C);
+    scanf("%d", &D);
+    Dif = A*B-C*D;
+    printf("DIFERENCA = %d\n", Dif);
+ 
+    return 0;
+}
